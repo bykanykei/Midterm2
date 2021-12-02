b50b5ef719c8cb8f40817c29a0389f0a652463a4
bykanykei <92913132+bykanykei@users.noreply.github.com>
2021-12-02 14:50:29 +0600

    Midterm

---
+++ app/src/main/java/kg/dani/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package kg.dani.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void Click(View view) {
+        startActivity(new Intent(this, MainActivity2.class));
+    }
+}
